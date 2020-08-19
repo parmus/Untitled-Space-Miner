77994b68d70ec9d94388ca7ac386a9b0b3383315
Martin Parm <parmus@gmail.com>
2020-08-20 01:45:15 +0200

    Merge SelectUnderCursor into MiningTool

--- Assets/_Project/Systems/ShuttleSystems/Scripts/MiningTool/MiningTool.cs
+++ Assets/_Project/Systems/ShuttleSystems/Scripts/MiningTool/MiningTool.cs
@@ -1,8 +1,11 @@
-﻿using SpaceGame.AsteroidSystem;
+﻿using System.Numerics;
+using SpaceGame.AsteroidSystem;
 using SpaceGame.Message_UI_System;
 using SpaceGame.ShuttleSystems.MiningTool.VFX;
 using SpaceGame.Utility;
 using UnityEngine;
+using UnityEngine.Assertions;
+using Vector3 = UnityEngine.Vector3;
 
 namespace SpaceGame.ShuttleSystems.MiningTool {
     [AddComponentMenu("ShuttleSystems/MiningTool")]
@@ -10,60 +13,87 @@ namespace SpaceGame.ShuttleSystems.MiningTool {
         #region Serialized fields
         [SerializeField] private Transform _origin = default;
         [SerializeField] private DefaultMiningTool _defaultMiningTool = new DefaultMiningTool();
+        [SerializeField] private LayerMask _layerMask = default;
         #endregion
 
         #region Properties
-
         public IReadonlyObservable<bool> InRange => _inRange;
         public readonly IObservable<IMiningToolConfiguration> Upgrade = new Observable<IMiningToolConfiguration>();
         #endregion
 
-        #region Private properties and variables
+        #region Private variables
+        private static readonly Vector3 ScreenCenter = new Vector3(0.5f, 0.5f);
+        private readonly Observable<GameObject> _targetGameObject = new Observable<GameObject>();
         private readonly Observable<bool> _inRange = new Observable<bool>();
-        private bool Use => _shuttle.ShuttleControls.Fire;
-        private IMiningToolConfiguration CurrentMiningTool => Upgrade.Value ?? _defaultMiningTool;
+        private Camera _camera;
+        private Ray _ray;
+        private RaycastHit _hit;
+        private Transform _cameraTransform;
+        private Vector3 _laserTarget = Vector3.zero;
         private MiningToolVFX _vfx = default;
         private ResourceDeposit _target = null;
         private Shuttle _shuttle;
         #endregion
 
+        #region Private properties
+        private bool Use => _shuttle.ShuttleControls.Fire;
+        private IMiningToolConfiguration CurrentMiningTool => Upgrade.Value ?? _defaultMiningTool;
+        #endregion
+
+        #region Unity hooks
         private void Awake() {
             _shuttle = GetComponent<Shuttle>();
             Upgrade.OnChange += upgrade => {
                 if (_vfx) Destroy(_vfx.gameObject);
                 _vfx = Instantiate(CurrentMiningTool.VFXPrefab, _origin);
             };
-
-            SelectUnderCursor.Target.OnChange += OnTargetChange;
-            OnTargetChange(SelectUnderCursor.Target.Value);
+            
+            _camera = Camera.main;
+            Assert.IsNotNull(_camera);
+            _cameraTransform = _camera.transform;
+            _targetGameObject.OnChange += OnTargetChange;  
         }
 
         private void Start() {
             if (!_vfx) _vfx = Instantiate(CurrentMiningTool.VFXPrefab, _origin);
         }
 
-        private void OnDestroy() => SelectUnderCursor.Target.OnChange -= OnTargetChange;
-
-        private void OnTargetChange(GameObject target) => _target = target ? target.GetComponentInParent<ResourceDeposit>() : null;
-
         private void OnDisable() {
             UpdateInRange();
             UpdateVFX();
         }
 
+        private void Reset() => _origin = transform;
+        #endregion
+
+        
+        private void OnTargetChange(GameObject target) => _target = target ? target.GetComponentInParent<ResourceDeposit>() : null;
+
+
+        private void UpdateTarget()
+        {
+            _ray = _camera.ViewportPointToRay(ScreenCenter);
+            var range = CurrentMiningTool.Range;
+            var hit = Physics.Raycast(_ray, out _hit, range, _layerMask);
+            _targetGameObject.Value = hit ? _hit.collider.gameObject : null;
+            _laserTarget = hit ? _hit.point : _cameraTransform.position + _cameraTransform.forward * range;
+        }
+
         private void UpdateInRange() {
-            _inRange.Value = enabled && _target && Vector3.Distance(_origin.position, _target.transform.position) < CurrentMiningTool.Range;
+            _inRange.Value = enabled && _target && Vector3.Distance(_origin.position, _laserTarget) < CurrentMiningTool.Range;
         }
 
         private void UpdateVFX() {
             if (!_vfx) return;
             var isFiringLaser = enabled && Use && !_shuttle.PowerSystem.IsEmpty;
             _vfx.IsMining = isFiringLaser;
-            _vfx.TargetPosition = SelectUnderCursor.Hit;
+            _vfx.TargetPosition = _laserTarget;
             _vfx.IsHitting = isFiringLaser && _inRange.Value;
         }
 
-        private void Update() {
+        private void Update()
+        {
+            UpdateTarget();
             UpdateInRange();
             UpdateVFX();
             if (!Use || _shuttle.PowerSystem.IsEmpty) return;
@@ -75,9 +105,6 @@ namespace SpaceGame.ShuttleSystems.MiningTool {
                 : $"+{resourceAcquired} {_target.Type.Name}");
         }
 
-        private void Reset() {
-            _origin = transform;
-        }
 
         [System.Serializable]
         private class DefaultMiningTool: IMiningToolConfiguration {
