c835e0cd6139fea73078716dd994f82d40313338
Martin Parm <parmus@gmail.com>
2020-09-01 13:12:06 +0200

    Replace LowPowerVolumeEffect with generic VolumeDriver

---
+++ Assets/_Project/Systems/Utility/Scripts/VolumeDriver.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace SpaceGame.Utility
+{
+    [RequireComponent(typeof(Volume))]
+    public class VolumeDriver : MonoBehaviour
+    {
+        [SerializeField] private AnimationCurve _curve = AnimationCurve.Linear(0, 0, 1, 1);
+        
+        private Volume _volume;
+
+        private void Awake() => _volume = GetComponent<Volume>();
+
+        public void SetWeight(float weight) => _volume.weight = _curve.Evaluate(weight);
+    }
+}
