42790f32033b1df6937e4c42573256d24f757391
Martin Parm <parmus@gmail.com>
2020-09-08 21:38:16 +0200

    Make Rotate more accurate and flexible

---
+++ Assets/_Project/Systems/Utility/Scripts/SimpleRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SpaceGame.Utility
+{
+    public class SimpleRotate : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed = 30f;
+        [SerializeField] private Vector3 _rotationAxis = Vector3.up;
+        [SerializeField] private Space _relativeTo = Space.Self;
+    
+        private Transform _transform = default;
+
+        private void Awake() => _transform = transform;
+
+        private void Update() => _transform.Rotate(_rotationAxis * (_rotationSpeed * Time.deltaTime), _relativeTo);
+    }
+}
